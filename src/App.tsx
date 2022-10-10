00f2e7673502c0de43b153de588d06157fd8ccfb
Andy Wright <Andy.Wright@theaccessgroup.com>
2022-10-10 17:42:43 +0100

    make todo-item handle remove and done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { shallowEqual } from 'react-redux';
-import { add, done, remove, Todo } from './todoSlice';
 
-import { useAppDispatch, useAppSelector } from './hooks';
+import { useAppSelector } from './hooks';
 import { TodoItem } from './components/todo-item';
 import { TodoForm } from './components/todo-form';
 
@@ -10,15 +9,6 @@ import './App.css';
 export const App = (): JSX.Element => {
   const count = useAppSelector((state) => state.todo.count, shallowEqual);
   const todos = useAppSelector((state) => state.todo.todos, shallowEqual);
-  const dispatch = useAppDispatch();
-
-  const handleRemove = (id: string): void => {
-    dispatch(remove(id));
-  }
-
-  const handleDone = (id: string): void => {
-    dispatch(done(id));
-  };
 
   return (
     <div className="App">
@@ -31,8 +21,6 @@ export const App = (): JSX.Element => {
           <TodoItem
             key={todo.id}
             todo={todo}
-            handleChange={() => handleDone(todo.id)}
-            handleRemove={() => handleRemove(todo.id)}
           />
         ))}
       </ul>
--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,25 +1,40 @@
 import classnames from 'classnames';
 
 import './todo-item.css';
-import { Todo } from '../todoSlice';
+import { done, remove, Todo } from '../todoSlice';
+import { useAppDispatch } from '../hooks';
 
-type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   todo: Todo,
-  handleChange: () => void,
-  handleRemove: (id: string) => void,
 }
 
-export const TodoItem = (props: Props): JSX.Element => (
-  <li className={classnames('todo', { done: props.todo.done })}>
-    <input
-      type="checkbox"
-      name={props.todo.id}
-      id={props.todo.id}
-      onChange={props.handleChange}
-    />
-    <label htmlFor={props.todo.id}>{props.todo.text}</label>
-    <div className="buttons">
-      <button type="button" className="danger" onClick={() => props.handleRemove(props.todo.id)}>&times;</button>
-    </div>
-  </li>
-);
+export const TodoItem = (props: Props): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const handleRemove = (id: string): void => {
+    dispatch(remove(id));
+  }
+
+  const handleDone = (id: string): void => {
+    dispatch(done(id));
+  };
+
+  return (
+    <li className={classnames('todo', { done: props.todo.done })}>
+      <input
+        type="checkbox"
+        name={props.todo.id}
+        id={props.todo.id}
+        onChange={() => handleDone(props.todo.id)}
+      />
+      <label htmlFor={props.todo.id}>{props.todo.text}</label>
+      <div className="buttons">
+        <button
+          type="button"
+          className="danger"
+          onClick={() => handleRemove(props.todo.id)}
+        >&times;</button>
+      </div>
+    </li>
+  );
+}
